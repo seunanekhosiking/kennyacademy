03b04c4820c693ea987ae98ccf7750d37681f257
seunanekhosiking <140523469+seunanekhosiking@users.noreply.github.com>
2023-09-01 12:43:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-// Define a function to generate HTML dynamically
-function generateDynamicHTML() {
-    // Create an HTML string
-    const htmlString = `
-        <h1>Welcome to My Dynamic Page</h1>
-        <p>This content is generated dynamically using JavaScript.</p>
-    `;
+// JavaScript code to change the text color when a button is clicked
+const button = document.querySelector('button');
+const text = document.querySelector('p');
 
-    // Get the "content" div element
-    const contentDiv = document.getElementById("content");
-
-    // Set the innerHTML of the div to the generated HTML
-    contentDiv.innerHTML = htmlString;
-}
-
-// Call the function to generate the HTML when the page loads
-window.addEventListener("load", generateDynamicHTML);
+button.addEventListener('click', () => {
+    text.style.color = 'red';
+});
