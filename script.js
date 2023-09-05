ecf622b6d46215bc014fb0865c7ab23790a62838
seunanekhosiking <140523469+seunanekhosiking@users.noreply.github.com>
2023-09-05 18:15:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-document.getElementById('whatsappForm').addEventListener('submit', function (e) {
+document.getElementById('registrationForm').addEventListener('submit', function (e) {
     e.preventDefault();
-    
+
     const name = document.getElementById('name').value;
     const surname = document.getElementById('surname').value;
     const email = document.getElementById('email').value;
-    
-    // Phone number to send the message to
+
+    // Phone number to send the message to (replace with your WhatsApp number)
     const phoneNumber = '+27789214737';
-    
+
+    // Create a WhatsApp message with registration details
+    const whatsappMessage = `Hi, I'm interested in registering for KEYDEN Computer Academy. My name is ${name} ${surname}, and my email is ${email}.`;
+
     // Create a WhatsApp message link
-    const whatsappMessage = `Hi, my name is ${name} ${surname}, and my email is ${email}.`;
     const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(whatsappMessage)}`;
-    
+
     // Redirect the user to WhatsApp
     window.location.href = whatsappLink;
 });
