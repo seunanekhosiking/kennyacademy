9af4df7b84db01f30de3a749f79d369acfc5758f
seunanekhosiking <140523469+seunanekhosiking@users.noreply.github.com>
2023-09-05 17:38:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-// JavaScript code to change the text color when a button is clicked
-const button = document.querySelector('button');
-const text = document.querySelector('p');
-
-button.addEventListener('click', () => {
-    text.style.color = 'red';
+document.getElementById('whatsappForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const surname = document.getElementById('surname').value;
+    const email = document.getElementById('email').value;
+    
+    // Create a WhatsApp message link
+    const whatsappMessage = `Hi, my name is ${name} ${surname}, and my email is ${email}.`;
+    const whatsappLink = `https://wa.me/?text=${encodeURIComponent(whatsappMessage)}`;
+    
+    // Redirect the user to WhatsApp
+    window.location.href = whatsappLink;
 });
