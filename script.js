73fe24ca0c064e2c530ba626bdf8eeab524e4047
seunanekhosiking <140523469+seunanekhosiking@users.noreply.github.com>
2023-09-05 17:46:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ document.getElementById('whatsappForm').addEventListener('submit', function (e) 
     const surname = document.getElementById('surname').value;
     const email = document.getElementById('email').value;
     
+    // Phone number to send the message to
+    const phoneNumber = '+27789214737';
+    
     // Create a WhatsApp message link
     const whatsappMessage = `Hi, my name is ${name} ${surname}, and my email is ${email}.`;
-    const whatsappLink = `https://wa.me/?text=${encodeURIComponent(whatsappMessage)}`;
+    const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(whatsappMessage)}`;
     
     // Redirect the user to WhatsApp
     window.location.href = whatsappLink;
